5407ac00d4968bbc1f559da69ed37ce1abdc5611
chxyuuu <carolchen952006@gmail.com>
2025-05-29 17:08:51 +0800

    add: test game setup

---
+++ test/test_game.py
@@ -0,0 +1,9 @@
+import unittest
+from game import Game
+from sprites.player import Player
+
+class TestGame(unittest.TestCase):
+    def setUp(self):
+        self.game = Game()
+        self.player = Player("TestPlayer")
+        self.game.add_player(self.player) # 假設 Game 有 add_player 方法
