b90d891abd09a6ccb8f1fca2de51845a69d036e6
chxyuuu <carolchen952006@gmail.com>
2025-05-29 17:09:47 +0800

    add: test game initialization

--- test/test_game.py
+++ test/test_game.py
@@ -7,3 +7,6 @@ class TestGame(unittest.TestCase):
         self.game = Game()
         self.player = Player("TestPlayer")
         self.game.add_player(self.player) # 假設 Game 有 add_player 方法
+    def test_initialization(self):
+        self.assertFalse(self.game.is_running)
+       
