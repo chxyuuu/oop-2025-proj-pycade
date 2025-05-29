03c6acf10e24f3889e56cdd8088b440410b868c4
chxyuuu <carolchen952006@gmail.com>
2025-05-29 17:24:45 +0800

    add: test menu loop

--- test/test_menu.py
+++ test/test_menu.py
@@ -23,3 +23,4 @@ class TestMenu(unittest.TestCase):
         self.assertEqual(self.menu.selected_option_index, 2)
         # 測試循環到開頭
         self.menu.move_selection_down()
+        self.assertEqual(self.menu.selected_option_index, 0)
